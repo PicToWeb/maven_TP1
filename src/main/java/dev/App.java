aed885e21b7ed9e7f0a483c46fd27405cb3134db
PicToWeb <picronantoine34@gmail.com>
2024-05-22 15:56:05 +0200

    tp03

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,22 @@
 package dev;
 
-/**
- * Hello world!
- *
- */
+import java.util.ResourceBundle;
+
+
+import com.github.lalyos.jfiglet.FigletFont;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World2!" );
+    	try {
+    		String titre = ResourceBundle.getBundle("application").getString("titre");
+    		String asciiArt = FigletFont.convertOneLine(titre);
+    		System.out.println(asciiArt);
+    		String titre2 = ResourceBundle.getBundle("application").getString("environnement");
+    		System.out.println("environnement" + titre2);
+    	}catch(Exception e){
+    		System.err.println(e.getMessage());
+    	}
+       
     }
 }
